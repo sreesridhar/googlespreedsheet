cf7d56dede2dc93c6e27e67215f39d4812c6de15
sridhar <sree.sridhar06@gmail.com>
2023-07-04 22:46:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var form = document.getElementById("sheetdb-form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  document.getElementById("submitBtn").disabled = true;
   fetch(form.action, {
     method: "POST",
     body: new FormData(document.getElementById("sheetdb-form")),
   })
     .then((response) => response.json())
     .then((html) => {
+      document.getElementById("submitBtn").disabled = false;
       window.open("thanks.html", "_blank");
     });
 });
